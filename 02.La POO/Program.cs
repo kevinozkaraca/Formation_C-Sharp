84838dad5cce4b6c13e0c7d46ab1bddd5456b29a
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2023-03-31 14:41:28 +0300

    apprendre encore

--- 02.La POO/Program.cs
+++ 02.La POO/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+---------  LES OBJETS 
+un objet est une instance d'une classe.
+Un objet est une variable particulière dont le type est une classe.
+Une classe est un type abstrait (exemple : un compte bancaire en général), 
+un objet est un exemplaire concret d'une classe (exemple : le compte bancaire de Jean).
+Tout le code écrit dans la section de base est écrit dans la class Program.
+*/
+
+// Voir le fichier ProgramPOO.cs
